d619c9c435695236d6e21d5abfd60613d61e76ab
BrianVegaSol <brivegasol@gmail.com>
2024-11-01 22:19:00 -0700

    Added Order Object to plan out how to handle order

--- src/main/java/com/pluarlsight/Order.java
+++ src/main/java/com/pluarlsight/Order.java
@@ -1,12 +1,18 @@
 package com.pluarlsight;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
 public class Order{
     StringBuilder sb = new StringBuilder();
-
+    HashMap<LocalDateTime, Order> orders;
+    private Bread bread;
+    public Order (Bread bread, Toppings toppings) {
+        this.bread = bread;
+    }
 
 
 //Super long toString for Checkout
-
 public String formatReceipt (Bread sand, Toppings top) {
     return sb.append("s")//.append(var).
             //.append()

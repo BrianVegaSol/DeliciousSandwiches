753bbe106617d531352c96530a118ee93bf7a9a4
BrianVegaSol <brivegasol@gmail.com>
2024-11-03 18:44:55 -0700

    Seems that checkout() is displaying properly now! :D

--- src/main/java/com/pluarlsight/UserInterface.java
+++ src/main/java/com/pluarlsight/UserInterface.java
@@ -177,6 +177,8 @@ o Cancel - delete order and go back to the home screen
                         //TODO When backing out, sout Did you want something else?
                         checkout();
                         break;
+                    //TODO BONUS??? Remove order and fix indices of all order! :) , remember to --itemOrderNumber!
+                        //case 5:
                     default:
                         System.out.println("Sorry! That's not on the menu!\nIs there anything else you would like?");
                         continue;
@@ -211,9 +213,12 @@ o Cancel - delete order and go back to the home screen
                 } else {
                     //FIXME if ordersMap == null then sout It doesn't look like you've ordered anything yet!
                     Order.ordersMap.forEach((key, value) ->
-                            homeSB.append("\n").append("toString Order#: ").append(key).
+                            homeSB.append("\n").append("toString Order#: ").append(key + 1).
                                     append(value));
                     System.out.println(homeSB + "\n");
+                    System.out.println("Index -1" + Order.ordersMap.get(-1));
+                    System.out.println("Index 0" +Order.ordersMap.get(0));
+                    System.out.println("Index 1" +Order.ordersMap.get(1));
                     homeSB.setLength(0);
                 }
                 break;

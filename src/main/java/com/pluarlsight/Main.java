c0bed5d781b6e644bf6ab3d32cb411ed9bb5cf5c
BrianVegaSol <brivegasol@gmail.com>
2024-11-15 10:44:34 -0700

    FINAL REVIEW VERSION

--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -1,7 +1,7 @@
 package com.pluarlsight;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         HomeMenu.homeMenu();
     }
 }
